8f17ab0fd992befe4d73964b97e9aa68c98a2e8c
BartoszRumak <27711689+RumakBartosz@users.noreply.github.com>
2021-03-28 16:57:47 +0200

    Add internal structure and replace simple function in evaluation

--- src/evaluation.erl
+++ src/evaluation.erl
@@ -2,73 +2,81 @@
 
 -export([evaluate/2]).
 
+%%====================================================================
+%% API functions
+%%====================================================================
 
 -spec evaluate(type:color(), type:tron_map()) -> integer().
 
 evaluate(MyColor, Map) ->
-    IAmLost = bot_utility:game_over(MyColor, Map),
-    HeIsLost = bot_utility:game_over(bot_utility:negate_color(MyColor), Map),
-    if
-        IAmLost ->
-            -1000;
-        HeIsLost ->
-            1000;
-        true ->
-            0
+    [{blue, BlueCloser}, {red, RedCloser}] = count_closer(Map),
+    case MyColor =:= blue of
+        true -> BlueCloser/RedCloser;
+        false -> RedCloser/BlueCloser
     end.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
 
 % get Voronoi diagram, check if there is distance 1 between any colored elements.
 %-spec isClosed(type:tron_map()) -> boolean().
 
-% compute moves, not euclidean distance
--spec getDistance(type:position(), type:position()) -> integer().
 
-getDistance({Xone, Yone}, {Xtwo, Ytwo}) ->
+% compute moves, not euclidean distance
+-spec get_distance(type:position(), type:position()) -> integer().
+
+get_distance({Xone, Yone}, {Xtwo, Ytwo}) ->
     (Xone - Xtwo) + (Yone - Ytwo).
 
-% start with bruteforce Voronoi, for every element of map, count distance to two heads.
--spec countCloser(type:tron_map()) -> [{blue | red, integer()}, ...].
 
-countCloser(Map) ->
-    countCloser(Map, [{blue, 0}, {red, 0}], 1).
+% start with bruteforce, for every element of map count distance to two heads.
+-spec count_closer(type:tron_map()) -> [{blue | red, integer()}, ...].
 
--spec countCloser(type:tron_map(), CloserCounter, integer()) ->
+count_closer(Map) ->
+    count_closer(Map, [{blue, 0}, {red, 0}], 1).
+
+
+-spec count_closer(type:tron_map(), CloserCounter, integer()) ->
                   CloserCounter when CloserCounter :: [{blue | red, integer()}, ...].
 
 % TODO: Both functions to refactor
-countCloser([], Acc, _VerticalAcc) ->
+count_closer([], Acc, _VerticalAcc) ->
     Acc;
-countCloser([Row|Rest] = Map, [{blue, CloserBlue}, {red, CloserRed}], VerticalAcc) ->
+count_closer([Row|Rest] = Map, [{blue, CloserBlue}, {red, CloserRed}], VerticalAcc) ->
     BlueHead = map_utility:get_head(blue, Map),
     RedHead = map_utility:get_head(red, Map),
-    [{blue, RowBlue}, {red, RowRed}] = countCloserVertical(Row,
-                                                           BlueHead,
-                                                           RedHead,
-                                                           {1, VerticalAcc},
-                                                           [{blue, 0}, {red, 0}]),
-    countCloser(Rest, [{blue, CloserBlue + RowBlue}, {red, CloserRed + RowRed}], VerticalAcc + 1).
+    [{blue, RowBlue}, {red, RowRed}] = count_closer_vertical(Row,
+                                                             BlueHead,
+                                                             RedHead,
+                                                             {1, VerticalAcc},
+                                                             [{blue, 0}, {red, 0}]),
+    count_closer(Rest, [{blue, CloserBlue + RowBlue}, {red, CloserRed + RowRed}], VerticalAcc + 1).
 
--spec countCloserVertical([type:tile()],
+
+-spec count_closer_vertical([type:tile()],
                            type:position(),
                            type:position(),
                            type:position(),
                            CloserCounter) ->
                            CloserCounter when CloserCounter :: [{blue | red, integer()}, ...].
 
-countCloserVertical([_Tile|Tiles],
+count_closer_vertical([], _BlueHead, _RedHead, _CurrentHead, Acc) ->
+    Acc;
+count_closer_vertical([_Tile|Tiles],
                     {BlueX, BlueY},
                     {RedX, RedY},
                     {TileX, TileY},
                     [{blue, CloserBlue}, {red, CloserRed}]) ->
-    DistanceToBlue = getDistance({BlueX, BlueY}, {TileX, TileY}),
-    DistanceToRed = getDistance({RedX, RedY}, {TileX, TileY}),
+    DistanceToBlue = get_distance({BlueX, BlueY}, {TileX, TileY}),
+    DistanceToRed = get_distance({RedX, RedY}, {TileX, TileY}),
     case DistanceToBlue < DistanceToRed of
-        true -> countCloserVertical(Tiles,
+        true -> count_closer_vertical(Tiles,
                                     {BlueX, BlueY},
                                     {RedX, RedY},
                                     {TileX, TileY},
                                     [{blue, CloserBlue + 1}, {red, CloserRed}]);
-        false -> countCloserVertical(Tiles,
+        false -> count_closer_vertical(Tiles,
                                      {BlueX, BlueY},
                                      {RedX, RedY},
                                      {TileX, TileY},
